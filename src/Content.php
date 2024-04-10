e96bc71effc5ee2b4873579172eac2dd65f150a2
User <y_zuev@mail.ru>
2024-04-10 03:46:37 +0300

    20240410#11

--- src/Content.php
+++ src/Content.php
@@ -81,7 +81,7 @@ class Content implements TemplateInterface {
 		foreach ($resource as $resKey => $resVal){
 			$this->{$resKey} = $resVal;
 		}
-		if(empty($incFile) || !file_exists(ContentData::$subtemplatePath.$incFile)) throw new AppError("Empty inc file ".$incFile);
+		if(empty($incFile) || !file_exists(ContentData::$subtemplatePath.$incFile)) throw new AppError("Inc file ".$incFile." is not found");
 		ob_start(); include ContentData::$subtemplatePath.$incFile; $out = ob_get_contents(); ob_end_clean();
 		return $out;
 	}
